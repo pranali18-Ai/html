65d398470bbb08030f054020e5847a7bbc728a44
Pranali <patilpranali0018@gmail.com>
2023-07-12 16:48:41 +0530

    HTML file commited

---
+++ popular_post.js
@@ -0,0 +1,35 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Less than 0.5")
+//     console.log(random);
+// }
+// else {
+//     console.log("Greater than 0.5")
+//     console.log(random);
+// }
+
+//Password length
+// const password = prompt("Please Enter Password");
+// if (password.length <= 6) {
+//     console.log("Password must be of 6 characters");
+// }
+// else {
+//     console.log("Good Password");
+// }
+
+// //Spaces in password
+// if (password.indexOf(' ') === -1) {
+//     console.log("No Spaces in Password");
+// }
+// else {
+//     alert("spaces in the password are not allowed");
+// }
+
+//Logical Operators
+// const password = prompt("Enter the Password");
+// if (password.length && password.indexOf(' ') === -1) {
+//     console.log("VALID PASSWORD");
+// }
+// else {
+//     console.log("INVALID PASSWORD");
+// }
